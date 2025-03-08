c3d4eea1e9f46b217f6c123eacd708053a77446a
Clifford-Joshua <155918426+Clifford-Joshua@users.noreply.github.com>
2025-03-08 07:17:31 +0100

    loginpage

--- src/Pages/LoginPage/Login.jsx
+++ src/Pages/LoginPage/Login.jsx
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+import login from "../../assets/login.svg";
+import { useAuth0 } from "@auth0/auth0-react";
+
 const Login = () => {
-  return <div>Login</div>;
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <Wrapper className="w-full h-screen flex flex-col items-center justify-center gap-9">
+      <img src={login} alt="Login illustration" />
+      <button
+        className="capitalize p-2  hover:text-white rounded-lg font-bold cursor-pointer text-[1.3rem] border border-blue-500 hover:bg-cyan-500 shadow-lg shadow-cyan-500/50"
+        onClick={loginWithRedirect}
+      >
+        Login/Signup
+      </button>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  /* ============================================================ */
+  /* Mobile View */
+`;
 
 export default Login;
